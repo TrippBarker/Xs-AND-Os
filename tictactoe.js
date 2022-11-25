f7829fc03715e86e9502a09e74a1ac4f3714a16b
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2022-11-25 03:15:00 +0000

    Win and ties work

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,10 @@
 // HTML ELEMENTS
 const squares = document.querySelectorAll('button');
 
-let lastRand = "";
 let xTurn = true;
 let marked = false;
 let winner = "";
+let counter = 0;
 
 let boardTracker = {
   a1: "",
@@ -33,14 +33,18 @@ function squareState(key){
   }
   boardTracker[key.id] = mark;
   key.classList.add("drawXO");
-  key.style.backgroundImage = "url(/resources/"+mark+"s/"+mark+Math.floor(Math.random() * 10)+".png)";
+  key.style.backgroundImage = "url(/resources/"+mark+"s/"+mark+(Math.floor(Math.random() * 10) + 1)+".png)";
 }
 
 function canClick(){
-  if (boardTracker[this.id] == ""){
+  if (boardTracker[this.id] == "" && winner == ""){
+    counter++;
     squareState(this);
+    if (counter == 9){
+      winner = "TIE";
+    }
+    checkForWin();
   }
-  checkForWin();
 }
 
 function checkSquareState(){
@@ -48,21 +52,21 @@ function checkSquareState(){
 }
 
 function checkForWin(){
-  if (boardTracker["a1"] && boardTracker["a2"] && boardTracker["a3"] == boardTracker["a1"]){
+  if (boardTracker["a2"] == boardTracker["a1"] && boardTracker["a3"] == boardTracker["a1"]){
     winner = boardTracker["a1"];
-  } else if (boardTracker["b1"] && boardTracker["b2"] && boardTracker["b3"] == boardTracker["b1"]){
+  } else if (boardTracker["b2"] == boardTracker["b1"]  && boardTracker["b3"] == boardTracker["b1"]){
     winner = boardTracker["b1"];
-  } else if (boardTracker["c1"] && boardTracker["c2"] && boardTracker["c3"] == boardTracker["c1"]){
+  } else if (boardTracker["c2"] == boardTracker["c1"] && boardTracker["c3"] == boardTracker["c1"]){
     winner = boardTracker["c1"];
-  } else if (boardTracker["a1"] && boardTracker["c1"] && boardTracker["b1"] == boardTracker["a1"]){
+  } else if (boardTracker["c1"]== boardTracker["a1"] && boardTracker["b1"] == boardTracker["a1"]){
     winner = boardTracker["a1"];
-  } else if (boardTracker["a2"] && boardTracker["c2"] && boardTracker["b2"] == boardTracker["a2"]){
+  } else if (boardTracker["c2"] == boardTracker["a2"] && boardTracker["b2"] == boardTracker["a2"]){
     winner = boardTracker["a2"];
-  } else if (boardTracker["a3"] && boardTracker["c3"] && boardTracker["b3"] == boardTracker["a3"]){
+  } else if (boardTracker["c3"] == boardTracker["a3"] && boardTracker["b3"] == boardTracker["a3"]){
     winner = boardTracker["a3"];
-  } else if (boardTracker["a1"] && boardTracker["b2"] && boardTracker["c3"] == boardTracker["a1"]){
+  } else if (boardTracker["b2"] == boardTracker["a1"] && boardTracker["c3"] == boardTracker["a1"]){
     winner = boardTracker["a1"];
-  } else if (boardTracker["a3"] && boardTracker["b2"] && boardTracker["c1"] == boardTracker["a3"]){
+  } else if (boardTracker["b2"] == boardTracker["a3"] && boardTracker["c1"] == boardTracker["a3"]){
     winner = boardTracker["a3"];
   }
   if (winner != ""){
