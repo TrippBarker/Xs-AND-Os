c8d2b64f8bccea5333c34e9d83de47d82fff2aff
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2022-11-27 17:23:41 +0000

    Score works now

--- tictactoe.js
+++ tictactoe.js
@@ -12,33 +12,38 @@ let counter;
 let selectedSquare;
 let availableSquares;
 let boardTracker;
-
-
+let xWTracker = 0;
+let oWTracker = 0;
+let tieWTracker = 0;
+let scoreToPass;
+  
 // FUNCTIONS
 
-function userSquareSelection(){
+function userSquareSelection() {
   selectedSquare = this.id;
   console.log(selectedSquare);
   checkIfSquareEmpty();
-  randXMovement();
+  if (xTurn) {
+    randXMovement();
+  }
 }
 
-function checkIfSquareEmpty(){
-  if (boardTracker[selectedSquare] == "" && winner == ""){
+function checkIfSquareEmpty() {
+  if (boardTracker[selectedSquare] == "" && winner == "") {
     counter++;
     drawMove();
     availableSquares.splice(availableSquares.indexOf(selectedSquare), 1);
     console.log(availableSquares);
-    if (counter == 9){
+    if (counter == 9) {
       winner = "TIE";
     }
     checkForWin();
   }
 }
 
-function drawMove(){
+function drawMove() {
   let mark = "x";
-  if (!xTurn){
+  if (!xTurn) {
     mark = "o";
     xTurn = true;
   } else {
@@ -46,54 +51,69 @@ function drawMove(){
   }
   boardTracker[selectedSquare] = mark;
   document.getElementById(selectedSquare).classList.add("drawXO");
-  document.getElementById(selectedSquare).style.backgroundImage = "url(./resources/"+mark+"s/"+mark+(Math.floor(Math.random() * 10) + 1)+".png)";
+  document.getElementById(selectedSquare).style.backgroundImage = "url(./resources/" + mark + "s/" + mark + (Math.floor(Math.random() * 10) + 1) + ".png)";
 }
 
-function checkForWin(){
-  if (boardTracker["a2"] == boardTracker["a1"] && boardTracker["a3"] == boardTracker["a1"] && boardTracker["a1"] != ""){
+function checkForWin() {
+  if (boardTracker["a2"] == boardTracker["a1"] && boardTracker["a3"] == boardTracker["a1"] && boardTracker["a1"] != "") {
     winner = boardTracker["a1"];
-  } else if (boardTracker["b2"] == boardTracker["b1"] && boardTracker["b3"] == boardTracker["b1"] && boardTracker["b1"] != ""){
+  } else if (boardTracker["b2"] == boardTracker["b1"] && boardTracker["b3"] == boardTracker["b1"] && boardTracker["b1"] != "") {
     winner = boardTracker["b1"];
-  } else if (boardTracker["c2"] == boardTracker["c1"] && boardTracker["c3"] == boardTracker["c1"] && boardTracker["c1"] != ""){
+  } else if (boardTracker["c2"] == boardTracker["c1"] && boardTracker["c3"] == boardTracker["c1"] && boardTracker["c1"] != "") {
     winner = boardTracker["c1"];
-  } else if (boardTracker["c1"]== boardTracker["a1"] && boardTracker["b1"] == boardTracker["a1"] && boardTracker["a1"] != ""){
+  } else if (boardTracker["c1"] == boardTracker["a1"] && boardTracker["b1"] == boardTracker["a1"] && boardTracker["a1"] != "") {
     winner = boardTracker["a1"];
-  } else if (boardTracker["c2"] == boardTracker["a2"] && boardTracker["b2"] == boardTracker["a2"] && boardTracker["a2"] != ""){
+  } else if (boardTracker["c2"] == boardTracker["a2"] && boardTracker["b2"] == boardTracker["a2"] && boardTracker["a2"] != "") {
     winner = boardTracker["a2"];
-  } else if (boardTracker["c3"] == boardTracker["a3"] && boardTracker["b3"] == boardTracker["a3"] && boardTracker["a3"] != ""){
+  } else if (boardTracker["c3"] == boardTracker["a3"] && boardTracker["b3"] == boardTracker["a3"] && boardTracker["a3"] != "") {
     winner = boardTracker["a3"];
-  } else if (boardTracker["b2"] == boardTracker["a1"] && boardTracker["c3"] == boardTracker["a1"] && boardTracker["a1"] != ""){
+  } else if (boardTracker["b2"] == boardTracker["a1"] && boardTracker["c3"] == boardTracker["a1"] && boardTracker["a1"] != "") {
     winner = boardTracker["a1"];
-  } else if (boardTracker["b2"] == boardTracker["a3"] && boardTracker["c1"] == boardTracker["a3"] && boardTracker["a3"] != ""){
+  } else if (boardTracker["b2"] == boardTracker["a3"] && boardTracker["c1"] == boardTracker["a3"] && boardTracker["a3"] != "") {
     winner = boardTracker["a3"];
   }
-  if (winner != ""){
+  if (winner != "") {
+    if (winner == "x") {
+      xWTracker++;
+      scoreToPass = xWTracker;
+    } else if (winner == "o") {
+      oWTracker++;
+      scoreToPass = oWTracker;
+    } else {
+      tieWTracker++;
+      scoreToPass = tieWTracker;
+    }
     message.textContent = "winner is " + winner;
+    updateScore(winner);
   }
 }
 
-function randXMovement(){
+function updateScore(winner){
+  document.getElementById(winner+"Score").textContent = winner + " Score: " + scoreToPass;
+}
+
+function randXMovement() {
   selectedSquare = availableSquares[Math.floor(Math.random() * availableSquares.length)];
   checkIfSquareEmpty();
 }
 
-function toggleStartMenu(){
+function toggleStartMenu() {
   document.getElementById("startMenu").classList.toggle("hidden");
   newGame();
 }
 
-function assignGridPos(square){
+function assignGridPos(square) {
   square.style.gridArea = square.id;
 }
 
-function newGame(){
+function newGame() {
   xTurn = true;
   winner = "";
   counter = 0;
   selectedSquare = "";
   availableSquares = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-  boardTracker = {a1: "", a2: "", a3: "", b1: "", b2: "", b3: "", c1: "", c2: "", c3: ""}
-  squares.forEach(square => square.style.backgroundImage="none");
+  boardTracker = { a1: "", a2: "", a3: "", b1: "", b2: "", b3: "", c1: "", c2: "", c3: "" };
+  squares.forEach(square => square.style.backgroundImage = "none");
   message.textContent = "";
   randXMovement();
 }
